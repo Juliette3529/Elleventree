61d7c1c488f48811fed231e1acf6c6e8f2377093
Juliette <juliette@saint-jalme.fr>
2020-04-06 15:40:28 +0200

    data affiched from json

--- web/public/scripts/main.js
+++ web/public/scripts/main.js
@@ -1,37 +1,44 @@
 $(document).ready(function () {
-    var swiper = new Swiper('.swiper-container', {
-        spaceBetween: 30,
-        autoplay: {
-            delay: 5000,
+  //const json = loadJsonData()
+  var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      autoplay: {
+          delay: 5000,
+        },
+        on: {
+          init: function () {
+            console.log('swiper initialized');
           },
-          on: {
-            init: function () {
-              console.log('swiper initialized');
-            },
-            slideChange : SlideActive,
-          },
-      });
-      function SlideActive(){
-          if($(this).find('.swiper-slide-active')){
-            // recup num slide
-              
-            //+ tard go chercher json + mets image en fond
-            //  $(this).find('.swiper-slide-active').addClass
-            console.log("page changée")
+          slideChange : SlideActive,
+        },
+    });
+    function SlideActive(){
+            //recup class des next slides
+            // envoyer pour chaque class le json qui convient
+          if(document.getElementsByClassName('trash swiper-slide-next').item(0) != null){
+
           }
+      console.log("page changée");
       }
-      SlideActive();
+      async function getJsonAsync(url, myInit) 
+      {
+        let response = await fetch(url , myInit);
+        let data = await response.json();
+        for( var key in data) {
+          console.log(data[0].title)
+            $('.title').html(data[0].title);
+            $('.name').html(data[0].name);
+        }
+        return data;
+      }
       var myHeaders = new Headers();
-      var myInit = { method: 'GET',
-               headers: myHeaders,
-               mode: 'no-cors',
-               cache: 'default' };
-      fetch('public/data/json/informations.json', myInit)
-      .then(function(response) {
-        response.json().then(function(parsedJson) {
-          console.log(parsedJson);
-        })
-      })
-        
+        var myInit = { method: 'GET',
+                     headers: myHeaders,
+                     mode: 'no-cors',
+                     cache: 'default' };
+      getJsonAsync('public/data/json/informations.json', myInit)
+        .then(function (data){
+          console.log(data);
+        }); 
 
 });
