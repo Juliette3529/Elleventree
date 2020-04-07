54bc04584848b334b134cc92a6f553e3a49fc7e4
Juliette <juliette@saint-jalme.fr>
2020-04-07 13:47:45 +0200

    ajout data dynamic on slide 3

--- web/public/scripts/main.js
+++ web/public/scripts/main.js
@@ -1,44 +1,51 @@
 $(document).ready(function () {
-  //const json = loadJsonData()
   var swiper = new Swiper('.swiper-container', {
-      spaceBetween: 30,
-      autoplay: {
-          delay: 5000,
-        },
-        on: {
-          init: function () {
-            console.log('swiper initialized');
-          },
-          slideChange : SlideActive,
-        },
-    });
-    function SlideActive(){
-            //recup class des next slides
-            // envoyer pour chaque class le json qui convient
-          if(document.getElementsByClassName('trash swiper-slide-next').item(0) != null){
-
-          }
-      console.log("page changée");
-      }
-      async function getJsonAsync(url, myInit) 
-      {
-        let response = await fetch(url , myInit);
-        let data = await response.json();
-        for( var key in data) {
-          console.log(data[0].title)
-            $('.title').html(data[0].title);
-            $('.name').html(data[0].name);
-        }
-        return data;
-      }
-      var myHeaders = new Headers();
-        var myInit = { method: 'GET',
-                     headers: myHeaders,
-                     mode: 'no-cors',
-                     cache: 'default' };
-      getJsonAsync('public/data/json/informations.json', myInit)
-        .then(function (data){
-          console.log(data);
-        }); 
+    spaceBetween: 30,
+    autoplay: {
+      delay: 5000,
+    },
+    on: {
+      init: function () {
+        console.log('swiper initialized');
+      },
+      slideChange: SlideActive,
+    },
+  });
+  function SlideActive() {
+    //recup class des next slides
+    // envoyer pour chaque class le json qui convient
+    if (document.getElementsByClassName('trash swiper-slide-next').item(0) != null) {
+      //trouver numero de la slide pour mettre le 1 comme ref id
+      getDataForSlide("trash");
+    }
+    if (document.getElementsByClassName('earth swiper-slide-next').item(0) != null) {
+      //trouver numero de la slide pour mettre le 1 comme ref id
+      getDataForSlide("earth");
+    }
+    console.log("page changée");
+  }
+  async function getJsonAsync(url, myInit, type) {
+    let response = await fetch(url, myInit);
+    let data = await response.json();
+    for (var key in data) {
+      $('.title').text(data[type].title);
+      $('.number').text(data[type].numberKey);
+      $('.text').text(data[type].text);
+      $('.background-swiper-slide').css("background-image", "url(" + data[type].pic + ")");
+    }
+    return data;
+  }
+  function getDataForSlide(type) {
+    var myHeaders = new Headers();
+    var myInit = {
+      method: 'GET',
+      headers: myHeaders,
+      mode: 'no-cors',
+      cache: 'default'
+    };
+    getJsonAsync('public/data/json/informations.json', myInit, type)
+      .then(function (data) {
+      });
+  }
 
 });
--- web/public/scripts/slide3.js
+++ web/public/scripts/slide3.js
@@ -1,15 +1,6 @@
-
 var i= 7775271220;
-
-//function format(i, sep) {
-//    i += '';
-//   var l = i.length;
-//   return i.substring(0, l % 3) + i.substring(l % 3).replace(reg, sep + '$1');
-//}
-
 function augmenter() {
 		i = i+3;
-		document.getElementById("nbHumain").innerHTML = i;
+		document.getElementById("numberhuman").innerHTML = i;
 	}
     setInterval("augmenter()", 1000);
-    
